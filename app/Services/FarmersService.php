9ce8f9d2c563195c9dc42cf8fa11cc18b46b6b31
Bernard D. White <danquahwhite@gmail.com>
2019-04-13 07:09:31 +0000

    Implemented farmers service class

--- app/Services/FarmersService.php
+++ app/Services/FarmersService.php
@@ -4,7 +4,20 @@
 namespace App\Services;
 
 
+use App\Http\traits\ConsumesExternalService;
+
 class FarmersService
 {
+    use ConsumesExternalService;
 
+    /**
+     * The base url to consume the farmers service
+     * @var string
+     */
+    public $baseUri;
+
+    public function __construct()
+    {
+        $this->baseUri = config('services.farmers.base_uri');
+    }
 }
